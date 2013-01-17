c87ec88f22ba39a3b8fe0acfb85f6d5b1c3829f3
Ron Huang <ronhuang@avamagic.com>
2013-01-17 12:29:36 +0800

    Initial import

---
+++ README.md
@@ -0,0 +1,24 @@
+MGServer Web API
+----------------
+
+Web application that exports RESTful API for MGServer.
+
+Installation
+============
+
+Use virtualenv.
+
+You can install the latest development snapshot like so:
+
+    git clone git@github.com:Avamagic/mgserver-web-api
+    cd mgserver-web-api
+    python setup.py install
+
+Development
+===========
+
+Source code is hosted on `GitHub <https://github.com/Avamagic/mgserver-web-api>`.
+
+    git clone git@github.com:Avamagic/mgserver-web-api
+    cd mgserver-web-api
+    python setup.py develop
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+MGServer Web API
+----------------
+
+Web application that exports RESTful API for MGServer
+"""
+
+from setuptools import setup
+
+setup(
+    name='MGServer',
+    version='0.0.1',
+    url='https://www.github.com/Avamagic/mgserver-web-api/',
+    license='BSD',
+    author='Ron Huang',
+    author_email='ronhuang@avamagic.com',
+    description='Web application that exports RESTful API for MGServer',
+    long_description=__doc__,
+    packages=['mgserver'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+        'Flask-Bcrypt',
+        'flask-oauthprovider',
+        'Flask-RESTful',
+        'Flask-PyMongo',
+    ]
+)
